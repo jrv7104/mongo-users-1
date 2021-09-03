57fe38435e288b9d94ac6e3a960e5e41017ebe1f
Manav Misra <manavm@visionwebsoft.com>
2021-09-02 20:52:08 -0500

    Add schema validation with 400 errors

--- app/router.js
+++ app/router.js
@@ -1,5 +1,6 @@
 import Router from "express";
 import usersController from "./controller.js";
+import userModel from "./model.js";
 
 const router = new Router();
 
@@ -9,16 +10,19 @@ router.get("/", async (_, res) => {
     const users = await usersController.index();
     res.json(users);
   } catch (error) {
+    // TODO: Send the errors back to the client
+    // TODO: Differentiate between 400 and 500 errors
     res.status(400).send(error);
   }
 });
 
 router.post("/", async (req, res) => {
   try {
-    const newUser = await usersController.add(req.body);
+    const validatedUser = userModel.createUser(req.body);
+    const newUser = await usersController.add(validatedUser);
     res.status(201).json(newUser);
   } catch (error) {
-    res.status(400);
+    res.status(400).send(error);
   }
 });
 
